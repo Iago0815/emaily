bb468fd7ea53b2ec22a282449f3cd6dec1ca64db
Marco Alvarado <marcoalvarado2@gmail.com>
2021-06-29 23:20:58 +0200

    typo correction

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,7 @@ module.exports = {
     "1092862111241-pgd8d687tcu08149dnifqc30figs3blg.apps.googleusercontent.com",
   googleClientSecret: "UTwgDj0kO_jaXx_C4rp8Mh4r",
   mongoURI:
-    "mongodb+srv://iago1234:jC4YNLuVY89SIn75@emaily.9k9bf.mongodb.net/my_emaily_db?retryWrites=true&w=majority",
+    "mongodb+srv://newiago1234:KhhAhHeH6hpa@emaily.9k9bf.mongodb.net/my_emaily_db?retryWrites=true&w=majority",
   cookieKey: "erfqergwrthwrthwrthwrthwrthwqer5",
   stripePublishableKey:
     "pk_test_51IaoVWDlWSgIVBHSsUEq0sfIsKFJ3I95olQnVKYzIxGNHncHqG7SseZRhaVZBBSSbpYXbL6L8IEJp1cbjc4xCU3400xZtuiDI5",
